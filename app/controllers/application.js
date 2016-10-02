12beb932c0522dd82861789ef4e0dc771031ac69
Matt McKenna <matt@mtmckenna.com>
2016-10-01 22:45:40 -0700

    use queryParamsHash addon

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,24 +1,15 @@
 import Ember from 'ember';
+import QueryParamsHash from 'ember-query-params-hash/mixins/query-params-hash';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(QueryParamsHash, {
   queryParams: ['period', 'scale'],
-
-  cubeAttributes: Ember.computed('period', 'scale', function() {
-    let params = this.get('queryParams');
-    let paramsHash = params.reduce((hash, attribute) => {
-      hash[attribute] = this.get(attribute);
-      return hash;
-    }, {});
-
-    return paramsHash;
-  }),
 
   controls: Ember.computed(function() {
     return [
       {
         name: 'period',
         displayName: 'Period',
-        value: this.get('period') || 0.0004,
+        value: this.get('period') || 0.0009,
         step: 0.0001,
         min: 0.0001,
         max: 0.0009
@@ -31,13 +22,14 @@ export default Ember.Controller.extend({
         min:0.1,
         max: 3.0
       }
-    ]
+    ];
   }),
 
   actions: {
     updateGrabBag(name, event) {
       let value = event.target.value;
       this.set(name, value);
+      this.notifyPropertyChange('queryParamsHash');
     }
   }
 });
