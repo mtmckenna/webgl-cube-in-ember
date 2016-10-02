4d5d62532d4630a877f514866c1ddaf7d30a1780
Matt McKenna <matt@mtmckenna.com>
2016-10-01 19:12:47 -0700

    use grab-bag-controls for sliders

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['period', 'scale'],
 
-  attributesHash: Ember.computed(function() {
+  cubeAttributes: Ember.computed('period', 'scale', function() {
     let params = this.get('queryParams');
     let paramsHash = params.reduce((hash, attribute) => {
       hash[attribute] = this.get(attribute);
@@ -11,13 +11,33 @@ export default Ember.Controller.extend({
     }, {});
 
     return paramsHash;
-  }).volatile(),
+  }),
+
+  controls: Ember.computed(function() {
+    return [
+      {
+        name: 'period',
+        displayName: 'Period',
+        value: this.get('period') || 0.0004,
+        step: 0.0001,
+        min: 0.0001,
+        max: 0.0009
+      },
+      {
+        name: 'scale',
+        displayName: 'Scale',
+        value: this.get('scale') || 1.0,
+        step: 0.1,
+        min:0.1,
+        max: 3.0
+      }
+    ]
+  }),
 
   actions: {
-    updateAttribute(name, event) {
+    updateGrabBag(name, event) {
       let value = event.target.value;
       this.set(name, value);
-      this.set('attributes', this.get('attributesHash'));
     }
   }
 });
